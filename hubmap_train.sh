bc5d6414b97715642e5baa0124ffb8802b170626
Dylan Wang <hwan7885@uni.sydney.edu.au>
2021-10-25 13:41:48 +1100

    v1.2

--- hubmap_train.sh
+++ hubmap_train.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-python train.py --freeze 10 --weights yolov5s.pt --data ./data/hubmap.yaml --img 640 --batch 64 --epochs 50 --device 1 --project ./runs/hubmap --name 10_s_640
+python train.py --weights yolov5s.pt --data ./data/hubmap.yaml --img 640 --batch 64 --epochs 300 --device 1 --project ./runs/hubmap --name all_s_640
 
-python train.py --freeze 24 --weights yolov5s.pt --data ./data/hubmap.yaml --img 640 --batch 64 --epochs 50 --device 1 --project ./runs/hubmap --name 24_s_640
+# python train.py --freeze 10 --weights yolov5s.pt --data ./data/hubmap.yaml --img 640 --batch 64 --epochs 50 --device 1 --project ./runs/hubmap --name 10_s_640
 
-python train.py --freeze 10 --weights yolov5s6.pt --data ./data/hubmap.yaml --img 1280 --batch 32 --epochs 50 --device 1 --project ./runs/hubmap --name 10_s_1280
+# python train.py --freeze 10 --weights yolov5s6.pt --data ./data/hubmap.yaml --img 1280 --batch 32 --epochs 50 --device 1 --project ./runs/hubmap --name 10_s_1280
 
-python train.py --freeze 24 --weights yolov5s6.pt --data ./data/hubmap.yaml --img 1280 --batch 32 --epochs 50 --device 1 --project ./runs/hubmap --name 24_s_1280
+# python train.py --freeze 24 --weights yolov5s.pt --data ./data/hubmap.yaml --img 640 --batch 64 --epochs 600 --device 1 --project ./runs/hubmap --name 24_s_640
+
+# python train.py --freeze 24 --weights yolov5s6.pt --data ./data/hubmap.yaml --img 1280 --batch 32 --epochs 300 --device 1 --project ./runs/hubmap --name 24_s_1280
+
+# python train.py --freeze 10 --weights yolov5x.pt --data ./data/hubmap.yaml --img 640 --batch 32 --epochs 50 --device 1 --project ./runs/hubmap --name 10_x_640
--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@
 
 # python train.py --freeze 24 --weights yolov5m.pt --data ./data/custom.yaml --img 640 --batch 64 --epochs 600 --device 1
 
-python train.py --freeze 10 --weights yolov5s6.pt --data ./data/custom.yaml --img 1280 --batch 32 --epochs 600 --device 1
+# python train.py --freeze 10 --weights yolov5s6.pt --data ./data/custom.yaml --img 1280 --batch 32 --epochs 600 --device 1
 
 # python train.py --freeze 24 --weights yolov5s6.pt --data ./data/custom.yaml --img 1280 --batch 64 --epochs 600 --device 1
